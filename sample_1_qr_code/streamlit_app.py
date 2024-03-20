b18246697ca29ab59c150d25f961081df6f5ff7a
terapyon <Manabu TERADA>
2024-03-21 07:56:56 +0900

    コードの微修正

--- sample_1_qr_code/streamlit_app.py
+++ sample_1_qr_code/streamlit_app.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 import streamlit as st
-import qrcode
+import qrcode  # type: ignore
 
 
 st.title("QRコード生成・確認アプリ")
@@ -8,15 +8,7 @@ st.text("QRコードの生成やQRコード画像の確認を行います")
 
 
 @st.cache_data
-def generate_qr_code(text: str) -> bytes:
-    with BytesIO() as output:
-        img.save(output, format="PNG")
-        img_value = output.getvalue()
-        return img_value
-
-
-text = st.text_input("QRコードに変換したい文字列を入力してください")
-if text:
+def generate_qr_code(text) -> bytes:
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -26,12 +18,20 @@ if text:
     qr.add_data(text)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-    if st.button("QRコードを確認"): 
-        img_value = generate_qr_code(text)
-        st.image(img_value, use_column_width=True)
-        st.download_button(
-            "QRコードをダウンロード",
-            data=img_value,
-            file_name="qr_code.png",
-            mime="image/png",
-        )
+    with BytesIO() as output:
+        img.save(output, format="PNG")
+        img_value = output.getvalue()
+    return img_value
+
+
+text = st.text_input("QRコードに変換したい文字列を入力してください")
+if text or st.button("QRコードを確認"):
+    img_value = generate_qr_code(text)
+    st.image(img_value, width=150)
+    st.text(f"QRコード文字列: {text}")
+    st.download_button(
+        "QRコードをダウンロード",
+        data=img_value,
+        file_name="qr_code.png",
+        mime="image/png",
+    )
